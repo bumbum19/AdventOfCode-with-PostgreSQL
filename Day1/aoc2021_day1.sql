dea52d5b7dff30fe1a91fd857229b369f2c32939
fjunginger <64534242+bumbum19@users.noreply.github.com>
2023-01-09 07:26:21 +0100

    Add files via upload

---
+++ Day1/aoc2021_day1.sql
@@ -0,0 +1,66 @@
+ 
+CREATE EXTENSION file_fdw;
+ 
+CREATE SERVER aoc2021 FOREIGN  DATA wrapper file_fdw;
+ 
+ 
+ 
+CREATE FOREIGN TABLE aoc2021_day1 (depth INT)
+SERVER aoc2021 options(filename 'D:\aoc2021.day1.input');
+
+ 
+ 
+ 
+ 
+ CREATE TEMPORARY TABLE sea_floor (
+  id  SERIAL,
+  depth  INT
+  
+);
+
+
+INSERT INTO sea_floor(depth)
+SELECT depth FROM aoc2021_day1;
+
+-- Part 1
+
+
+
+WITH cte AS
+(
+ SELECT depth, 
+ LAG(depth) OVER 
+	(ORDER BY id) AS prev_depth FROM sea_floor 
+)
+ 
+SELECT 
+SUM(CASE WHEN depth > prev_depth THEN 1 END) AS answer 
+FROM cte;
+
+
+
+-- Solution: 1298
+
+
+
+
+-- Part 2
+
+WITH cte AS
+
+(
+ SELECT SUM(depth) OVER 
+	(ORDER BY id RANGE BETWEEN 2 PRECEDING AND CURRENT ROW ) 
+	 AS depth FROM sea_floor OFFSET 2
+),
+
+cte2 AS
+(
+ SELECT depth, 
+	LAG(depth) OVER () AS prev_depth FROM cte 
+)
+ 
+SELECT 
+SUM(CASE WHEN depth > prev_depth THEN 1 END) AS 
+FROM cte2;
+
