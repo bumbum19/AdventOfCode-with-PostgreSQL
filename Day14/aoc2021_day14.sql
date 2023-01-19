6653c12954c87b51b96833d5f0ba409dff347d4e
fjunginger <64534242+bumbum19@users.noreply.github.com>
2023-01-19 10:33:38 +0100

    Update aoc2021_day14.sql

--- Day14/aoc2021_day14.sql
+++ Day14/aoc2021_day14.sql
@@ -1,6 +1,68 @@
-SET lc_messages TO 'en_US.UTF-8';
+--- Day 14: Extended Polymerization ---
+
+/*
+
+The incredible pressures at this depth are starting to put a strain on your submarine. The submarine has polymerization equipment that would produce 
+suitable materials to reinforce the submarine, and the nearby volcanically-active caves should even have the necessary input elements in sufficient quantities.
+
+The submarine manual contains instructions for finding the optimal polymer formula; specifically, it offers a polymer template and a list of pair insertion rules
+(your puzzle input). You just need to work out what polymer would result after repeating the pair insertion process a few times.
+
+For example:
+
+NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C
+
+The first line is the polymer template - this is the starting point of the process.
+
+The following section defines the pair insertion rules. A rule like AB -> C means that when elements A and B are immediately adjacent, 
+element C should be inserted between them. These insertions all happen simultaneously.
+
+So, starting with the polymer template NNCB, the first step simultaneously considers all three pairs:
+
+    The first pair (NN) matches the rule NN -> C, so element C is inserted between the first N and the second N.
+    The second pair (NC) matches the rule NC -> B, so element B is inserted between the N and the C.
+    The third pair (CB) matches the rule CB -> H, so element H is inserted between the C and the B.
+
+Note that these pairs overlap: the second element of one pair is the first element of the next pair. Also, because all pairs are considered simultaneously, 
+inserted elements are not considered to be part of a pair until the next step.
+
+After the first step of this process, the polymer becomes NCNBCHB.
+
+Here are the results of a few steps using the above rules:
+
+Template:     NNCB
+After step 1: NCNBCHB
+After step 2: NBCCNBBBCBHCB
+After step 3: NBBBCNCCNBBNBNBBCHBHHBCHB
+After step 4: NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB
+
+This polymer grows quickly. After step 5, it has length 97; After step 10, it has length 3073. After step 10, B occurs 1749 times, C occurs 298 times, H occurs 161 times, 
+and N occurs 865 times; taking the quantity of the most common element (B, 1749) and subtracting the quantity of the least common element (H, 161) produces 1749 - 161 = 1588.
+
+Apply 10 steps of pair insertion to the polymer template and find the most and least common elements in the result. What do you get if you take the quantity of the
+most common element and subtract the quantity of the least common element?
+
+*/
 
 
+-- Setup
 
 CREATE FOREIGN TABLE aoc2021_day14 (x text)
  SERVER aoc2022 options(filename 'D:\aoc2021.day14.input');
@@ -18,17 +80,6 @@ SELECT 0, x FROM aoc2021_day14 WHERE
 x != '' AND x NOT LIKE '%->%';
 
 
-
-
-
-
-
-
-
-
-INSERT INTO polymer(day,x) VALUES (0,'NNCB');
-
-
 CREATE TEMPORARY TABLE  rules (
   id  SERIAL,
   adj  CHAR(2),
@@ -44,31 +95,7 @@ FROM aoc2021_day14
 WHERE 
 x != '' AND x  LIKE '%->%';
 
-
-
-
-
-
-INSERT INTO rules (adj, bet)
-VALUES
-('CH', 'B'),
-('HH', 'N'),
-('CB', 'H'),
-('NH', 'C'),
-('HB', 'C'),
-('HC', 'B'),
-('HN', 'C'),
-('NN', 'C'),
-('BH', 'H'),
-('NC', 'B'),
-('NB', 'B'),
-('BN', 'B'),
-('BB', 'N'),
-('BC', 'B'),
-('CC', 'N'),
-('CN', 'C');
-
--- Part 1
+-- Solution
  
 
 WITH RECURSIVE s AS 
@@ -87,29 +114,35 @@ FROM polymer CROSS JOIN  GENERATE_SERIES(1,LENGTH(x)-1) AS pos
 UNION ALL
 SELECT t.v2, s.v2, step + 1
 FROM t JOIN s ON
-t.v2 = s.v1 WHERE step < 8
+t.v2 = s.v1 WHERE step < 10
 ),
 
 cte AS 
 (
 
 SELECT SUBSTR(v2,2),COUNT(*) + CASE WHEN SUBSTR((SELECT x FROM polymer),1,1) = SUBSTR(v2,2) THEN 1 ELSE 0 END  AS cnt 
-FROM t WHERE step = 8 GROUP BY SUBSTR(v2,2) 
+FROM t WHERE step = 10 GROUP BY SUBSTR(v2,2) 
 )
-
-
-SELECT * FROM cte;
 
 SELECT MAX(cnt) - MIN(cnt) AS answer FROM cte;
 
 
--- Part 2
+--- Part Two ---
+
+/*
+The resulting polymer isn't nearly strong enough to reinforce the submarine. You'll need to run more steps of the pair insertion process; a total of 40 steps should do it.
+
+In the above example, the most common element is B (occurring 2192039569602 times) and the least common element is H (occurring 3849876073 times); 
+subtracting these produces 2188189693529.
+
+Apply 40 steps of pair insertion to the polymer template and find the most and least common elements in the result. What do you get if you take the quantity of the most common element and 
+subtract the quantity of the least common element?
+
+*/
 
 
 
-
-
-
+-- Solution
 
 WITH RECURSIVE s AS 
 (
@@ -163,20 +196,3 @@ GROUP BY SUBSTR(b.v2,2)
 )
 
 SELECT MAX(cnt) - MIN(cnt) AS answer FROM cte3;
-
-
-
-
-
-
-
-
-
-
-
-2965769177100
-
-47452306833600
-
-
-
