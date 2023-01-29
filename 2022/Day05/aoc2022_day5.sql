ea220e3d314f110eefcf8832d987c84fd34ab0fe
fjunginger <64534242+bumbum19@users.noreply.github.com>
2023-01-29 08:09:59 +0100

    Update aoc2022_day5.sql

--- 2022/Day05/aoc2022_day5.sql
+++ 2022/Day05/aoc2022_day5.sql
@@ -1,3 +1,76 @@
+--- Day 5: Supply Stacks ---
+
+/*
+
+The expedition can depart as soon as the final supplies have been unloaded from the ships. Supplies are stored in stacks of marked crates, 
+but because the needed supplies are buried under many other crates, the crates need to be rearranged.
+
+The ship has a giant cargo crane capable of moving crates between stacks. To ensure none of the crates get crushed or fall over, 
+the crane operator will rearrange them in a series of carefully-planned steps. After the crates are rearranged, the desired crates will be at the top of each stack.
+
+The Elves don't want to interrupt the crane operator during this delicate procedure, but they forgot to ask her which crate will end up where, 
+and they want to be ready to unload them as soon as possible so they can embark.
+
+They do, however, have a drawing of the starting stacks of crates and the rearrangement procedure (your puzzle input). For example:
+
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+
+In this example, there are three stacks of crates. Stack 1 contains two crates: crate Z is on the bottom, and crate N is on top. Stack 2 contains three crates; 
+from bottom to top, they are crates M, C, and D. Finally, stack 3 contains a single crate, P.
+
+Then, the rearrangement procedure is given. In each step of the procedure, a quantity of crates is moved from one stack to a different stack. 
+In the first step of the above rearrangement procedure, one crate is moved from stack 2 to stack 1, resulting in this configuration:
+
+[D]        
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+In the second step, three crates are moved from stack 1 to stack 3. Crates are moved one at a time, so the first crate to be moved (D) ends up 
+below the second and third crates:
+
+        [Z]
+        [N]
+    [C] [D]
+    [M] [P]
+ 1   2   3
+
+Then, both crates are moved from stack 2 to stack 1. Again, because crates are moved one at a time, crate C ends up below crate M:
+
+        [Z]
+        [N]
+[M]     [D]
+[C]     [P]
+ 1   2   3
+
+Finally, one crate is moved from stack 1 to stack 2:
+
+        [Z]
+        [N]
+        [D]
+[C] [M] [P]
+ 1   2   3
+
+The Elves just need to know which crate will end up on top of each stack; in this example, the top crates are C in stack 1, M in stack 2, and Z in stack 3, 
+so you should combine these together and give the Elves the message CMZ.
+
+After the rearrangement procedure completes, what crate ends up on top of each stack?
+
+
+
+
+*/
+
+
+
 CREATE FOREIGN TABLE aoc2022_day5 (x text)
 SERVER aoc2022 options(filename 'D:\aoc2022.day5.input');
 
@@ -10,18 +83,6 @@ height  INT,
 symbol text
  );
  
-
-INSERT INTO arrangement
-VALUES
-(1,1,'[Z]'),
-(1,2,'[N]'),
-(2,1,'[M]'),
-(2,2,'[C]'),
-(2,3,'[D]'),
-(3,1,'[P]');
-
-
-
 CREATE TEMPORARY TABLE  moves 
 (
 id SERIAL,
@@ -29,64 +90,33 @@ undock  INT,
 target INT,
 amount INT
  );
- 
-INSERT INTO moves(undock,target,amount)
-VALUES
-(2,1,1),
-(1,3,3),
-(2,1,2),
-(1,2,1);
-
-
-
---------------------
-
-
-CREATE TEMPORARY TABLE  arrangement 
-(
-stack INT,
-height  INT,
-symbol text
- );
- 
- 
-
-
 
 INSERT INTO arrangement
-WITH t AS 
+WITH cte AS 
+(
+	SELECT * 
+	FROM  GENERATE_SERIES (1,9) AS stack
+	CROSS JOIN GENERATE_SERIES (8,1,-1) AS height
+),
+
+cte2 AS 
 (
 
-SELECT * FROM  GENERATE_SERIES (1,9) AS stack CROSS JOIN GENERATE_SERIES (8,1,-1) AS height
+	SELECT x, ROW_NUMBER() OVER () AS rnk FROM  aoc2022_day5
+	WHERE x NOT LIKE 'move%' AND x NOT LIKE '%1%' AND x NOT LIKE ''
 ),
 
 
-t2 AS 
+cte3 AS 
 (
 
-SELECT x, ROW_NUMBER() OVER () AS rnk FROM  aoc2022_day5
-WHERE x NOT LIKE 'move%' AND x NOT LIKE '%1%' AND x NOT LIKE ''
-),
-
-
-t3 AS 
-(
-
-SELECT x, RANK() OVER (ORDER BY rnk DESC) AS height FROM t2
+	SELECT x, RANK() OVER (ORDER BY rnk DESC) AS height FROM cte2
 )
 
-SELECT stack, height, SUBSTR(x,stack*4 - 3,3) AS symbol FROM t3 NATURAL JOIN t
+SELECT stack, height, SUBSTR(x,stack*4 - 3,3) AS symbol 
+FROM cte3 NATURAL JOIN cte
 WHERE SUBSTR(x,stack*4 - 3,3) != '   '
 ; 
-
- 
- CREATE TEMPORARY TABLE  moves 
-(
-id SERIAL,
-undock  INT,
-target INT,
-amount INT
- );
 
 INSERT INTO moves (undock,target,amount)
 SELECT 
@@ -98,8 +128,117 @@ WHERE x LIKE 'move%'
 ;
   
   
- -- Solution 
 
+
+WITH RECURSIVE arrangement2(stack, height, symbol)  AS 
+(
+	SELECT 1, 0, 'bottom' 
+	UNION ALL
+	SELECT stack + 1 ,0, 'bottom' FROM arrangement2
+	WHERE stack < (SELECT MAX(stack) FROM arrangement)
+),
+
+arrangement3 AS 
+
+(
+	TABLE arrangement
+	UNION ALL
+	TABLE arrangement2
+),
+
+
+cte (step, stack, height, symbol, dep) AS 
+(
+	SELECT 0, *, MAX(height) OVER (PARTITION BY stack) 
+	FROM arrangement3
+	UNION ALL
+	
+	SELECT * FROM
+	(WITH cte_copy AS 
+		(SELECT * FROM cte) 
+	
+	SELECT step + 1,
+	CASE WHEN  stack = undock AND height > dep - amount AND height > 0 THEN target ELSE stack END , 
+	CASE WHEN  stack = undock AND height > dep - amount AND height > 0
+		THEN (SELECT DISTINCT dep FROM cte_copy WHERE stack = target) + dep - height + 1
+		ELSE height END , 
+	symbol, 
+	MAX(CASE WHEN  stack = undock AND height > dep - amount AND height > 0
+		THEN (SELECT DISTINCT dep FROM cte_copy WHERE stack = target) + dep - height  + 1
+		ELSE height END) OVER (PARTITION BY CASE WHEN  stack = undock AND height > dep - amount
+		THEN target ELSE stack END) FROM cte_copy JOIN moves ON step + 1 = id
+	
+	
+	) AS dt
+	
+)
+
+
+
+SELECT STRING_AGG(SUBSTR(symbol,2,1),'' ORDER BY stack) FROM cte 
+WHERE step = (SELECT COUNT(*) FROM moves) AND height > 0 AND height = dep;
+
+
+
+
+
+--- Part Two ---
+
+/*
+
+
+As you watch the crane operator expertly rearrange the crates, you notice the process isn't following your prediction.
+
+Some mud was covering the writing on the side of the crane, and you quickly wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
+
+The CrateMover 9001 is notable for many new and exciting features: air conditioning, leather seats, an extra cup holder, 
+and the ability to pick up and move multiple crates at once.
+
+Again considering the example above, the crates begin in the same configuration:
+
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+Moving a single crate from stack 2 to stack 1 behaves the same as before:
+
+[D]        
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+However, the action of moving three crates from stack 1 to stack 3 means that those three moved crates stay in the same order, resulting in this new configuration:
+
+        [D]
+        [N]
+    [C] [Z]
+    [M] [P]
+ 1   2   3
+
+Next, as both crates are moved from stack 2 to stack 1, they retain their order as well:
+
+        [D]
+        [N]
+[C]     [Z]
+[M]     [P]
+ 1   2   3
+
+Finally, a single crate is still moved from stack 1 to stack 2, but now it's crate C that gets moved:
+
+        [D]
+        [N]
+        [Z]
+[M] [C] [P]
+ 1   2   3
+
+In this example, the CrateMover 9001 has put the crates in a totally different order: MCD.
+
+Before the rearrangement process finishes, update your simulation so that the Elves know where they should stand to be ready to unload the final supplies. 
+After the rearrangement procedure completes, what crate ends up on top of each stack?
+
+
+*/
 
 WITH RECURSIVE arrangement2(stack, height, symbol )  AS 
 (
@@ -119,93 +258,36 @@ arrangement3 AS
 ),
 
 
-t (step, stack, height, symbol, dep) AS 
+cte (step, stack, height, symbol, dep) AS 
 (
 	SELECT 0, *, MAX(height) OVER (PARTITION BY stack) 
 	FROM arrangement3
 	UNION ALL
 	
 	SELECT * FROM
-	(WITH t_copy AS 
-		(SELECT * FROM t) 
+	(WITH cte_copy AS 
+		(SELECT * FROM cte) 
 	
 	SELECT step + 1,
 	CASE WHEN  stack = undock AND height > dep - amount AND height > 0 THEN target ELSE stack END , 
 	CASE WHEN  stack = undock AND height > dep - amount AND height > 0
-		THEN (SELECT DISTINCT dep FROM t_copy WHERE stack = target) + dep - height + 1
+		THEN (SELECT DISTINCT dep FROM cte_copy WHERE stack = target) + height - (dep - amount) 
 		ELSE height END , 
 	symbol, 
 	MAX(CASE WHEN  stack = undock AND height > dep - amount AND height > 0
-		THEN (SELECT DISTINCT dep FROM t_copy WHERE stack = target) + dep - height  + 1
+		THEN (SELECT DISTINCT dep FROM cte_copy WHERE stack = target) + height - (dep - amount) 
 		ELSE height END) OVER (PARTITION BY CASE WHEN  stack = undock AND height > dep - amount
-		THEN target ELSE stack END) FROM t_copy JOIN moves ON step + 1 = id
+		THEN target ELSE stack END) FROM cte_copy JOIN moves ON step + 1 = id
 	
 	
-	) AS t3
+	) AS dt
 	
 )
 
 
 
-SELECT STRING_AGG(SUBSTR(symbol,2,1),'' ORDER BY stack) FROM t 
-
-WHERE step = (SELECT COUNT(*) FROM moves) AND height > 0 AND height = dep;
-
-
-
-
-
--- Part 2
-
-
-WITH RECURSIVE arrangement2(stack, height, symbol )  AS 
-(
-	SELECT 1, 0, 'bottom' 
-	UNION ALL
-	SELECT stack + 1 ,0, 'bottom' FROM arrangement2
-	WHERE stack < (SELECT MAX(stack) FROM arrangement)
-	
-	
-),
-
-arrangement3 AS 
-
-(TABLE arrangement
- UNION ALL
- TABLE arrangement2
-),
-
-
-t (step, stack, height, symbol, dep) AS 
-(
-	SELECT 0, *, MAX(height) OVER (PARTITION BY stack) 
-	FROM arrangement3
-	UNION ALL
-	
-	SELECT * FROM
-	(WITH t_copy AS 
-		(SELECT * FROM t) 
-	
-	SELECT step + 1,
-	CASE WHEN  stack = undock AND height > dep - amount AND height > 0 THEN target ELSE stack END , 
-	CASE WHEN  stack = undock AND height > dep - amount AND height > 0
-		THEN (SELECT DISTINCT dep FROM t_copy WHERE stack = target) + height - (dep - amount) 
-		ELSE height END , 
-	symbol, 
-	MAX(CASE WHEN  stack = undock AND height > dep - amount AND height > 0
-		THEN (SELECT DISTINCT dep FROM t_copy WHERE stack = target) + height - (dep - amount) 
-		ELSE height END) OVER (PARTITION BY CASE WHEN  stack = undock AND height > dep - amount
-		THEN target ELSE stack END) FROM t_copy JOIN moves ON step + 1 = id
-	
-	
-	) AS t3
-	
-)
-
-
-
-SELECT STRING_AGG(SUBSTR(symbol,2,1),'' ORDER BY stack) FROM t 
-
+SELECT STRING_AGG(SUBSTR(symbol,2,1),'' ORDER BY stack) 
+FROM cte 
 WHERE step = (SELECT COUNT(*) FROM moves) AND height > 0 AND height = dep;
 
 
@@ -213,41 +295,3 @@ WHERE step = (SELECT COUNT(*) FROM moves) AND height > 0 AND height = dep;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-WITH t AS 
-(
-SELECT *, MAX(height) OVER (PARTITION BY stack) AS dep FROM arrangement, moves WHERE id = 1 
-)
-
-
-SELECT 
-CASE WHEN  stack = undock AND height > dep - amount THEN target ELSE stack END AS stack , 
-CASE WHEN  stack = undock AND height > dep - amount 
-	THEN (SELECT DISTINCT dep FROM t WHERE stack = target) + dep - height + 1
-	ELSE height END AS height, 
-symbol, 
-MAX(CASE WHEN  stack = undock AND height > dep - amount 
-	THEN (SELECT DISTINCT dep FROM t WHERE stack = target) + dep - height + 1
-	ELSE height END) OVER (PARTITION BY CASE WHEN  stack = undock AND height > dep - amount
-	THEN target ELSE stack END) AS dep FROM t 
-
-;
