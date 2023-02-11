41d953ea9b648915368227df76187b6f9b6e1f4b
fjunginger <64534242+bumbum19@users.noreply.github.com>
2023-02-11 11:03:04 +0100

    Update aoc2016_day2.sql

--- 2016/Day02/aoc2016_day2.sql
+++ 2016/Day02/aoc2016_day2.sql
@@ -9,7 +9,8 @@ Fancy office buildings like this one usually have keypad locks on their bathroom
 below to access the bathrooms."
 
 The document goes on to explain that each button to be pressed can be found by starting on the previous button and moving to adjacent buttons on the keypad: 
-U moves up, D moves down, L moves left, and R moves right. Each line of instructions corresponds to one button, starting at the previous button (or, for the first line, the "5" button); press whatever button you're on at the end of each line. If a move doesn't lead to a button, ignore it.
+U moves up, D moves down, L moves left, and R moves right. Each line of instructions corresponds to one button, starting at the previous button 
+(or, for the first line, the "5" button); press whatever button you're on at the end of each line. If a move doesn't lead to a button, ignore it.
 
 You can't hold it much longer, so you decide to figure out the code as you walk to the bathroom. You picture a keypad like this:
 
