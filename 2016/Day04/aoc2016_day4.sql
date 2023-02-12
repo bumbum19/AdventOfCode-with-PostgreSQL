e2b2a7a23bd06a10b0915d71eed018237c1e31dc
fjunginger <64534242+bumbum19@users.noreply.github.com>
2023-02-12 07:39:59 +0100

    Update aoc2016_day4.sql

--- 2016/Day04/aoc2016_day4.sql
+++ 2016/Day04/aoc2016_day4.sql
@@ -1,8 +1,37 @@
+--- Day 4: Security Through Obscurity ---
+
+/*
+
+
+Finally, you come across an information kiosk with a list of rooms. Of course, the list is encrypted and full of decoy data,
+but the instructions to decode the list are barely hidden nearby. Better remove the decoy data first.
+
+Each room consists of an encrypted name (lowercase letters separated by dashes) followed by a dash, a sector ID, and a checksum in square brackets.
+
+A room is real (not a decoy) if the checksum is the five most common letters in the encrypted name, in order, with ties broken by alphabetization. For example:
+
+   - aaaaa-bbb-z-y-x-123[abxyz] is a real room because the most common letters are a (5), b (3), and then a tie between x, y, and z, which are listed alphabetically.
+   - a-b-c-d-e-f-g-h-987[abcde] is a real room because although the letters are all tied (1 of each), the first five are listed alphabetically.
+   - not-a-real-room-404[oarel] is a real room.
+   - totally-real-room-200[decoy] is not.
+
+Of the real rooms from the list above, the sum of their sector IDs is 1514.
+
+What is the sum of the sector IDs of the real rooms?
+*/
+
+
+
+
+
+-- Read data
+
+
 CREATE FOREIGN TABLE aoc2016_day4 (x TEXT)
 SERVER aoc2016 options(filename 'D:\aoc2016.day4.input');
 
 
-
+-- Create base table
 
 CREATE  TEMPORARY TABLE rooms
 (
@@ -14,7 +43,7 @@ CREATE  TEMPORARY TABLE rooms
 );
 
 
-
+-- Insert data
 
 INSERT INTO rooms(encrypted_name, sector_id, check_sum)
 SELECT 
@@ -24,7 +53,7 @@ LEFT(SPLIT_PART(x,'[',2),-1)
 FROM aoc2016_day4;
 
 
--- Part 1
+-- First Star
 
 WITH cte AS 
 (
@@ -50,7 +79,28 @@ cte2 AS
 SELECT SUM(sector_id) FROM cte2 WHERE check_word = check_sum;
 
 
--- Part 2
+--- Part Two ---
+
+/*
+
+
+With all the decoy data out of the way, it's time to decrypt this list and get moving.
+
+The room names are encrypted by a state-of-the-art shift cipher, which is nearly unbreakable without the right software. 
+However, the information kiosk designers at Easter Bunny HQ were not expecting to deal with a master cryptographer like yourself.
+
+To decrypt a room name, rotate each letter forward through the alphabet a number of times equal to the room's sector ID. A becomes B, B becomes C, Z becomes A, and so on. 
+Dashes become spaces.
+
+For example, the real name for qzmt-zixmtkozy-ivhz-343 is very encrypted name.
+
+What is the sector ID of the room where North Pole objects are stored?
+*/
+
+
+
+
+-- Second Star
 
 
 WITH cte AS 
@@ -64,7 +114,6 @@ WITH cte AS
 ),
 
 cte2 AS 
-
 (
 
 	SELECT  id, sector_id, word_pos,
@@ -75,7 +124,8 @@ cte2 AS
 
 
 SELECT sector_id  FROM cte2
-GROUP BY id, sector_id HAVING STRING_AGG(word, ' ' ORDER BY word_pos) ILIKE  '%northpole%' ; 
+GROUP BY id, sector_id 
+HAVING STRING_AGG(word, ' ' ORDER BY word_pos) ILIKE  '%northpole%'; 
 
 
 
