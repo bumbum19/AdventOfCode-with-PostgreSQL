c401885786e92eddc12aab10dcc0440b0a646a20
fjunginger <64534242+bumbum19@users.noreply.github.com>
2023-02-14 07:42:54 +0100

    Update aoc2015_day3.sql

--- 2015/aoc2015_day3.sql
+++ 2015/aoc2015_day3.sql
@@ -1,8 +1,30 @@
+--- Day 3: Perfectly Spherical Houses in a Vacuum ---
+
+/*
+
+Santa is delivering presents to an infinite two-dimensional grid of houses.
+
+He begins by delivering a present to the house at his starting location, and then an elf at the North Pole calls him via radio and tells him where to move next. 
+Moves are always exactly one house to the north (^), south (v), east (>), or west (<). After each move, he delivers another present to the house at his new location.
+
+However, the elf back at the north pole has had a little too much eggnog, and so his directions are a little off, and Santa ends up visiting some houses more than once. 
+How many houses receive at least one present?
+
+For example:
+
+   - > delivers presents to 2 houses: one at the starting location, and one to the east.
+   - ^>v< delivers presents to 4 houses in a square, including twice to the house at his starting/ending location.
+   - ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
+
+*/
+
+-- Read data
+
 CREATE FOREIGN TABLE aoc2015_day3 (x TEXT)
 SERVER aoc2015 options(filename 'D:\aoc2015.day3.input');
 
 
-
+-- Create base table
 
 CREATE  TEMPORARY TABLE instructions
 (
@@ -13,13 +35,15 @@ CREATE  TEMPORARY TABLE instructions
 );
 
 
+-- Insert data
+
 INSERT INTO instructions(direction)
 SELECT STRING_TO_TABLE(x, NULL) FROM aoc2015_day3;
 
 
 
 
--- Part 1
+-- First Star
 
 
 WITH cte AS 
@@ -35,9 +59,9 @@ WITH cte AS
 cte2 AS 
 (
 
-SELECT DISTINCT ON (x,y) x,y FROM cte 
-UNION 
-VALUES (0,0)
+	SELECT DISTINCT ON (x,y) x,y FROM cte 
+	UNION 
+	VALUES (0,0)
 
 
 )
@@ -47,9 +71,27 @@ SELECT COUNT(*) FROM cte2;
 
 
 
+--- Part Two ---
 
--- Part 2
+/*
 
+The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to deliver presents with him.
+
+Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then take turns moving based on instructions from the elf, 
+who is eggnoggedly reading from the same script as the previous year.
+
+This year, how many houses receive at least one present?
+
+For example:
+
+   - ^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
+   - ^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
+   - ^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
+
+*/
+
+
+-- Second Star
 
 WITH santa AS 
 (
@@ -79,20 +121,14 @@ robo_santa AS
 combined AS 
 (
 
-SELECT DISTINCT ON (x,y) x,y FROM santa 
-UNION 
-SELECT DISTINCT ON (x,y) x,y FROM robo_santa 
-UNION
-VALUES (0,0)
+	SELECT DISTINCT ON (x,y) x,y FROM santa 
+	UNION 
+	SELECT DISTINCT ON (x,y) x,y FROM robo_santa 
+	UNION
+	VALUES (0,0)
 
 
 )
 
 SELECT COUNT(*) FROM combined;
-
-
-
-
-
-
 
