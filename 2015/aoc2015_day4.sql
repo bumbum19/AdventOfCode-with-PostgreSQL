c4d0ae8967088297fb1564f74a1ea6ade5e0db17
fjunginger <64534242+bumbum19@users.noreply.github.com>
2023-02-14 08:40:00 +0100

    Update aoc2015_day4.sql

--- 2015/aoc2015_day4.sql
+++ 2015/aoc2015_day4.sql
@@ -1,12 +1,29 @@
-SET lc_messages TO 'en_US.UTF-8';
+--- Day 4: The Ideal Stocking Stuffer ---
+
+/*
+
+Santa needs help mining some AdventCoins (very similar to bitcoins) to use as gifts for all the economically forward-thinking little girls and boys.
+
+To do this, he needs to find MD5 hashes which, in hexadecimal, start with at least five zeroes. The input to the MD5 hash is some secret key 
+(your puzzle input, given below) followed by a number in decimal. To mine AdventCoins, you must find Santa the lowest positive number (no leading zeroes: 1, 2, 3, ...) that produces such a hash.
+
+For example:
+
+   - If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043 starts with five zeroes (000001dbbfa...), 
+     and it is the lowest such number to do so.
+   - If your secret key is pqrstuv, the lowest number it combines with to make an MD5 hash starting with five zeroes is 1048970; that is, 
+     the MD5 hash of pqrstuv1048970 looks like 000006136ef....
+
+*/
 
 
+-- Read data
 
 CREATE FOREIGN TABLE aoc2015_day4 (x TEXT)
 SERVER aoc2015 options(filename 'D:\aoc2015.day4.input');
 
 
-
+-- Create base table
 
 CREATE TEMPORARY TABLE coin
 (
@@ -15,11 +32,14 @@ CREATE TEMPORARY TABLE coin
 );
 
 
+-- Insert data
+
 INSERT INTO coin 
 TABLE aoc2015_day4;
 
 
--- Part 1
+
+-- First Star
 
 SELECT id 
 FROM coin 
@@ -32,7 +52,18 @@ LIMIT 1;
 
 
 
--- Part 2
+--- Part Two ---
+
+
+
+/*
+
+Now find one that starts with six zeroes.
+
+*/
+
+
+-- Second Star
 
 SELECT id 
 FROM coin 
@@ -45,6 +76,3 @@ LIMIT 1;
 
 
 
-10000000
-254575
-1038736
