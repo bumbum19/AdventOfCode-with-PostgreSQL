c2c76fb704b6fe1555e1d8aec3657dbb69921874
Fabian Junginger <fjunginger90@gmail.com>
2023-02-24 11:15:43 +0100

    Update aoc2021_day12.sql

--- 2021/Day12/aoc2021_day12.sql
+++ 2021/Day12/aoc2021_day12.sql
@@ -104,16 +104,22 @@ start-RW
 How many paths through this cave system are there that visit small caves at most once?
 */
 
+-- Read data
+
 CREATE FOREIGN TABLE aoc2021_day12 (a text)
- SERVER aoc2022 options(filename 'D:\aoc2021.day12.input');
+SERVER aoc2022 options(filename 'D:\aoc2021.day12.input');
+
+
+-- Create base table
  
- 
-CREATE TEMPORARY TABLE  caves (
-id  SERIAL,
-v1  text, 
-v2 text
+CREATE TEMPORARY TABLE  caves 
+(
+	id  SERIAL,
+	v1  text, 
+	v2 text
 );
 
+-- Insert data
 
 INSERT INTO caves(v1,v2)
 SELECT 
@@ -121,34 +127,41 @@ SPLIT_PART(a,'-',1),
 SPLIT_PART(a,'-',2)
 FROM aoc2021_day12;
 
--- Solution
+-- First Star
 
 WITH RECURSIVE s AS 
-
-
-(SELECT v1, v2 FROM caves 
-WHERE v1 != 'end' AND v2 !=  'start'
-UNION ALL  
-SELECT v2, v1 FROM caves
-WHERE v1 !=  'start' AND v2 != 'end' )
+(
+	SELECT v1, v2 
+	FROM caves 
+	WHERE v1 != 'end' 
+	AND v2 !=  'start'
+	UNION ALL  
+	SELECT v2, v1 
+	FROM caves
+	WHERE v1 !=  'start' 
+	AND v2 != 'end' 
+)
 ,
 
  t(v1,v2, is_cycle, path) AS 
-
 (
 
-SELECT *, FALSE, ARRAY[s.v1]  
-FROM s WHERE v1 = 'start'
-UNION ALL
-SELECT s.v1, s.v2, CASE WHEN s.v1 = LOWER(s.v1) THEN s.v1 = ANY(path) ELSE FALSE END,
-    	CASE WHEN s.v2 != 'end' THEN path || s.v1 ELSE  path || s.v1  || s.v2 END
- FROM  s 
- JOIN t
- ON s.v1 = t.v2 AND NOT is_cycle
+	SELECT *, FALSE, ARRAY[s.v1]  
+	FROM s WHERE v1 = 'start'
+	UNION ALL
+	SELECT s.v1, s.v2, CASE WHEN s.v1 = LOWER(s.v1) THEN s.v1 = ANY(path) ELSE FALSE END,
+		CASE WHEN s.v2 != 'end' THEN path || s.v1 ELSE  path || s.v1  || s.v2 END
+	 FROM  s 
+	 JOIN t
+	 	ON s.v1 = t.v2 
+		AND NOT is_cycle
 ) 
 
 
-SELECT COUNT(path) FROM t WHERE NOT is_cycle AND v2 = 'end' ;
+SELECT COUNT(path) 
+FROM t 
+WHERE NOT is_cycle 
+AND v2 = 'end' ;
 
 
 --- Part Two ---
@@ -204,43 +217,37 @@ The slightly larger example above now has 103 paths through it, and the even lar
 Given these new rules, how many paths through this cave system are there?
 */
 
--- Solution
-
--- Query may take 20 seconds!!!
+-- Second Star
 
 WITH RECURSIVE s AS 
+(
+	SELECT v1, v2 FROM caves 
+	WHERE v1 != 'end' AND v2 !=  'start'
+	UNION ALL  
+	SELECT v2, v1 FROM caves 
+	WHERE v1 !=  'start' AND v2 != 'end'
+),
 
-(SELECT v1, v2 FROM caves 
-WHERE v1 != 'end' AND v2 !=  'start'
-UNION ALL  
-SELECT v2, v1 FROM caves 
-WHERE v1 !=  'start' AND v2 != 'end'
-)
-,
-
- t(v1,v2, is_cycle, dummy, path) AS 
-
+t(v1,v2, is_cycle, dummy, path) AS 
 (
 
-SELECT *, FALSE , FALSE, ARRAY[s.v1]  
-FROM s WHERE v1 = 'start'
-UNION ALL
-SELECT s.v1, s.v2, 
-		CASE WHEN s.v1 = LOWER(s.v1)  THEN s.v1 = ANY(path) 
-			 ELSE FALSE 
-		END,
-		CASE WHEN s.v1 = LOWER(s.v1) AND s.v1 = ANY(path) AND NOT dummy  THEN TRUE 
-		   WHEN s.v1 = LOWER(s.v1) AND s.v1 = ANY(path) AND dummy  THEN FALSE 
-		   ELSE dummy 
-		END,
-     CASE WHEN s.v2 != 'end' THEN path || s.v1 ELSE  path || s.v1  || s.v2 END
- FROM  s 
- JOIN t
- ON s.v1 = t.v2 AND (NOT is_cycle OR dummy)
+	SELECT *, FALSE , FALSE, ARRAY[s.v1]  
+	FROM s WHERE v1 = 'start'
+	UNION ALL
+	SELECT s.v1, s.v2, 
+	CASE WHEN s.v1 = LOWER(s.v1)  THEN s.v1 = ANY(path) 
+		 ELSE FALSE END,
+	CASE WHEN s.v1 = LOWER(s.v1) AND s.v1 = ANY(path) AND NOT dummy  THEN TRUE 
+	   WHEN s.v1 = LOWER(s.v1) AND s.v1 = ANY(path) AND dummy  THEN FALSE 
+	   ELSE dummy END,
+	CASE WHEN s.v2 != 'end' THEN path || s.v1 ELSE  path || s.v1  || s.v2 END
+	FROM  s 
+	JOIN t
+		ON s.v1 = t.v2 
+		AND (NOT is_cycle OR dummy)
 
 ) 
 
 SELECT COUNT(*) FROM t 
 WHERE (NOT is_cycle OR dummy) 
-AND v2 = 'end' 
-;
+AND v2 = 'end';
