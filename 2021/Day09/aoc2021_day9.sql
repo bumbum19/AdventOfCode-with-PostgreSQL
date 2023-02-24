d647fe258b26622ea75caf8e491a3d2fd0d12d7d
Fabian Junginger <fjunginger90@gmail.com>
2023-02-24 11:11:29 +0100

    Update aoc2021_day9.sql

--- 2021/Day09/aoc2021_day9.sql
+++ 2021/Day09/aoc2021_day9.sql
@@ -31,64 +31,69 @@ Find all of the low points on your heightmap. What is the sum of the risk levels
 */
 
 
+-- Read data
+
 CREATE FOREIGN TABLE aoc2021_day9 (a text)
- SERVER aoc2022 options(filename 'D:\aoc2021.day9.input');
+SERVER aoc2022 options(filename 'D:\aoc2021.day9.input');
 
 
-
+-- Create base table
  
 CREATE TEMPORARY TABLE  lava_tubes  
 (
-id  SERIAL ,
-heights  INT[]
+	id  SERIAL,
+	heights  INT[]
 );
+
+
+-- Insert data
+
+
 
   
 INSERT INTO lava_tubes(heights)
-SELECT STRING_TO_ARRAY(a, NULL)::INT[] FROM aoc2021_day9;
+SELECT STRING_TO_ARRAY(a, NULL)::INT[] 
+FROM aoc2021_day9;
 
 
--- Solution
+-- First Star
 
 	
 WITH cte AS 
 
 (
-SELECT id AS x, y, height
-FROM lava_tubes 
-CROSS JOIN UNNEST(heights) WITH ORDINALITY AS q(height, y)
+	SELECT id AS x, y, height
+	FROM lava_tubes 
+	CROSS JOIN UNNEST(heights) 
+		WITH ORDINALITY AS q(height, y)
 ),
 
 
 cte2 AS
 (
-SELECT *, 
-LAG(height) OVER (PARTITION BY x ORDER BY y) AS west,
-LEAD(height) OVER (PARTITION BY x ORDER BY y) AS east,
-LAG(height) OVER (PARTITION BY y ORDER BY x) AS north,
-LEAD(height) OVER (PARTITION BY y ORDER BY x) AS south
-
-FROM cte
+	SELECT *, 
+	LAG(height) OVER (PARTITION BY x ORDER BY y) AS west,
+	LEAD(height) OVER (PARTITION BY x ORDER BY y) AS east,
+	LAG(height) OVER (PARTITION BY y ORDER BY x) AS north,
+	LEAD(height) OVER (PARTITION BY y ORDER BY x) AS south
+        FROM cte
 ),
 
 
 cte3 AS 
-
 (
-SELECT *, 
-LAG(north) OVER (PARTITION BY x ORDER BY y) AS north_west,
-LEAD(north) OVER (PARTITION BY x ORDER BY y) AS north_east,
-LAG(south) OVER (PARTITION BY x ORDER BY y) AS south_west,
-LEAD(south) OVER (PARTITION BY x ORDER BY y) AS south_east
-
-FROM cte2
+	SELECT *, 
+	LAG(north) OVER (PARTITION BY x ORDER BY y) AS north_west,
+	LEAD(north) OVER (PARTITION BY x ORDER BY y) AS north_east,
+	LAG(south) OVER (PARTITION BY x ORDER BY y) AS south_west,
+	LEAD(south) OVER (PARTITION BY x ORDER BY y) AS south_east
+	FROM cte2
 )
 
 SELECT SUM(1 + height) AS answer FROM cte3  
-WHERE CASE  
-WHEN LEAST(east, west, north, south, north_east, 
+WHERE CASE WHEN LEAST(east, west, north, south, north_east, 
 	 north_west, south_east, south_west) >= height
-THEN TRUE ELSE FALSE END  ;
+		THEN TRUE ELSE FALSE END ;
 
 
 
@@ -144,65 +149,63 @@ What do you get if you multiply together the sizes of the three largest basins?
 
 */
 
--- Solution
-
--- Query may need about 5 seconds!!!
+-- Second Star
 
 WITH RECURSIVE cte AS 
-
 (
-SELECT id AS x, y, height
-FROM lava_tubes 
-CROSS JOIN UNNEST(heights) WITH ORDINALITY AS q(height, y)
+	SELECT id AS x, y, height
+	FROM lava_tubes 
+	CROSS JOIN UNNEST(heights) 
+		WITH ORDINALITY AS q(height, y)
 ),
-
 
 cte2 AS
 (
-SELECT *, 
-LAG(height) OVER (PARTITION BY x ORDER BY y) AS west,
-LEAD(height) OVER (PARTITION BY x ORDER BY y) AS east,
-LAG(height) OVER (PARTITION BY y ORDER BY x) AS north,
-LEAD(height) OVER (PARTITION BY y ORDER BY x) AS south
-
-FROM cte
+	SELECT *, 
+	LAG(height) OVER (PARTITION BY x ORDER BY y) AS west,
+	LEAD(height) OVER (PARTITION BY x ORDER BY y) AS east,
+	LAG(height) OVER (PARTITION BY y ORDER BY x) AS north,
+	LEAD(height) OVER (PARTITION BY y ORDER BY x) AS south
+	FROM cte
 ),
-
 
 t AS 
 
-(
-SELECT *, 
-LAG(north) OVER (PARTITION BY x ORDER BY y) AS north_west,
-LEAD(north) OVER (PARTITION BY x ORDER BY y) AS north_east,
-LAG(south) OVER (PARTITION BY x ORDER BY y) AS south_west,
-LEAD(south) OVER (PARTITION BY x ORDER BY y) AS south_east
-FROM cte2
+(	
+	SELECT *, 
+	LAG(north) OVER (PARTITION BY x ORDER BY y) AS north_west,
+	LEAD(north) OVER (PARTITION BY x ORDER BY y) AS north_east,
+	LAG(south) OVER (PARTITION BY x ORDER BY y) AS south_west,
+	LEAD(south) OVER (PARTITION BY x ORDER BY y) AS south_east
+	FROM cte2
 ),
 
-s (x, y, height, basin) AS 
+s(x, y, height, basin) AS 
 (
-SELECT x,y, height, ROW_NUMBER() OVER () 
-FROM t WHERE CASE  
-WHEN LEAST(east, west, north, south,north_east, 
-	   north_west, south_east, south_west) >= height
-THEN TRUE ELSE FALSE END
+	SELECT x,y, height, ROW_NUMBER() OVER () 
+	FROM t WHERE CASE  
+	WHEN LEAST(east, west, north, south,north_east, 
+		   north_west, south_east, south_west) >= height
+		THEN TRUE ELSE FALSE END
 
-UNION 
+	UNION 
 
-SELECT cte.x,cte.y, cte.height, basin
-FROM s JOIN cte 
-ON  cte.x = s.x AND cte.y IN (s.y - 1, s.y + 1 )
-OR  cte.y = s.y AND cte.x IN (s.x - 1, s.x + 1 )
-WHERE s.height <= cte.height
-AND cte.height < 9
-
+	SELECT cte.x,cte.y, cte.height, basin
+	FROM s JOIN cte 
+	ON  cte.x = s.x AND cte.y IN (s.y - 1, s.y + 1 )
+	OR  cte.y = s.y AND cte.x IN (s.x - 1, s.x + 1 )
+	WHERE s.height <= cte.height
+	AND cte.height < 9
 ),
 
 h AS 
 
 (
-SELECT COUNT(*) AS cnt FROM s  GROUP BY basin ORDER BY cnt DESC LIMIT 3
+	SELECT COUNT(*) AS cnt 
+	FROM s 
+	GROUP BY basin 
+	ORDER BY cnt DESC 
+	LIMIT 3
 )
 
 
