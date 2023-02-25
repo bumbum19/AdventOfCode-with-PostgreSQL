1e935adacf3e1a46ac1b02babb396dfc8f267a13
Fabian Junginger <fjunginger90@gmail.com>
2023-02-25 12:34:28 +0100

    Update aoc2017_day11.sql

--- 2017/aoc2017_day11.sql
+++ 2017/aoc2017_day11.sql
@@ -1,63 +1,63 @@
+--- Day 11: Hex Ed ---
+
+/*
+
+Crossing the bridge, you've barely reached the other side of the stream when a program comes up to you, clearly in distress. "It's my child process," she says, 
+"he's gotten lost in an infinite grid!"
+
+Fortunately for her, you have plenty of experience with infinite grids.
+
+Unfortunately for you, it's a hex grid.
+
+The hexagons ("hexes") in this grid are aligned such that adjacent hexes can be found to the north, northeast, southeast, south, southwest, and northwest:
+
+  \ n  /
+nw +--+ ne
+  /    \
+-+      +-
+  \    /
+sw +--+ se
+  / s  \
+
+You have the path the child process took. Starting where he started, you need to determine the fewest number of steps required to reach him. 
+(A "step" means to move from the hex you are in to any adjacent hex.)
+
+For example:
+
+   - ne,ne,ne is 3 steps away.
+   - ne,ne,sw,sw is 0 steps away (back where you started).
+   - ne,ne,s,s is 2 steps away (se,se).
+   - se,sw,se,sw,sw is 3 steps away (s,s,sw).
+*/
+
+
+-- Read data
+
 CREATE FOREIGN TABLE aoc2017_day11 (x TEXT)
 SERVER aoc2017 options(filename 'D:\aoc2017.day11.input');
 
 
 
+-- Create base table
+
 CREATE TEMPORARY TABLE hex_grid
 (
 	id SERIAL,
 	move TEXT
 	
 );
+
+-- Insert data
 
 INSERT INTO hex_grid(move)
 SELECT STRING_TO_TABLE(x, ',') FROM aoc2017_day11;
 
 
-----------------------------------------------
 
 
-CREATE TEMPORARY TABLE hex_grid
-(
-	id SERIAL,
-	move TEXT
-	
-);
+-- First Star
 
-INSERT INTO hex_grid(move)
-VALUES
-('se'),
-('sw'),
-('se'),
-('sw'),
-('sw');
-
----------------------
-CREATE TEMPORARY TABLE hex_grid
-(
-	id SERIAL,
-	move TEXT
-	
-);
-
-INSERT INTO hex_grid(move)
-VALUES
-('ne'),
-('ne'),
-('s'),
-('s');
-
-
-
-
-
-
-
-
-
-
-
--- Part 1
+-- Just doing some computations in a finetly generated abelian group
 
 
 WITH cte AS 
@@ -104,19 +104,22 @@ CASE WHEN n > s THEN
 			 END
 			 END
 	END 
-			
-			
-
-
-
 FROM cte;
 
 
 
 
 
--- Part 2
+--- Part Two ---
 
+/*
+
+How many steps away is the furthest he ever got from his starting position?
+
+*/
+
+
+-- Second Star
 
 WITH cte AS 
 (
@@ -166,6 +169,3 @@ CASE WHEN n > s THEN
 			 END
 	END 
 )) AS t(steps);
-
-		
-		
