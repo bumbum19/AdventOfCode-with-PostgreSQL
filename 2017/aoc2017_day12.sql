4351d4d28266cf6ef1b56b0fb7522163e8f1ea14
Fabian Junginger <fjunginger90@gmail.com>
2023-02-26 06:49:54 +0100

    Update aoc2017_day12.sql

--- 2017/aoc2017_day12.sql
+++ 2017/aoc2017_day12.sql
@@ -1,5 +1,48 @@
-SET lc_messages TO 'en_US.UTF-8';
+--- Day 12: Digital Plumber ---
 
+/*
+
+
+Walking along the memory banks of the stream, you find a small village that is experiencing a little confusion: some programs can't communicate with each other.
+
+Programs in this village communicate using a fixed system of pipes. Messages are passed between programs using these pipes, but most programs aren't connected 
+to each other directly. Instead, programs pass messages between each other until the message reaches the intended recipient.
+
+For some reason, though, some of these messages aren't ever reaching their intended recipient, and the programs suspect that some pipes are missing. 
+They would like you to investigate.
+
+You walk through the village and record the ID of each program and the IDs with which it can communicate directly (your puzzle input). 
+Each program has one or more programs with which it can communicate, and these pipes are bidirectional; if 8 says it can communicate with 11, 
+then 11 will say it can communicate with 8.
+
+You need to figure out how many programs are in the group that contains program ID 0.
+
+For example, suppose you go door-to-door like a travelling salesman and record the following list:
+
+0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5
+
+In this example, the following programs are in the group that contains program ID 0:
+
+   - Program 0 by definition.
+   - Program 2, directly connected to program 0.
+   - Program 3 via program 2.
+   - Program 4 via program 2.
+   - Program 5 via programs 6, then 4, then 2.
+   - Program 6 via programs 4, then 2.
+
+Therefore, a total of 6 programs are in this group; all but program 1, which has a pipe that connects it to itself.
+
+How many programs are in the group that contains program ID 0?
+*/
+
+
+-- Read data
 
 
 CREATE FOREIGN TABLE aoc2017_day12 (x TEXT)
@@ -7,12 +50,16 @@ SERVER aoc2017 options(filename 'D:\aoc2017.day12.input');
 
 
 
+-- Create base table
+
 CREATE TEMPORARY TABLE pipes
 (
 	id_1 INT,
 	id_2 INT,
 	PRIMARY KEY(id_1, id_2)
 );
+
+-- Insert data
 
 INSERT INTO pipes
 SELECT SPLIT_PART(x, '<->', 1)::INT , z::INT 
@@ -23,7 +70,7 @@ CROSS JOIN STRING_TO_TABLE(y, ',') AS z;
 
 
 
--- Part 1
+-- First star
 
  WITH RECURSIVE cte(id) AS
 (
@@ -39,11 +86,25 @@ CROSS JOIN STRING_TO_TABLE(y, ',') AS z;
 
 SELECT COUNT(*) FROM cte;
 
+--- Part Two ---
+
+/*
+
+There are more programs than just the ones in the group containing program ID 0. The rest of them have no way of reaching that group, 
+and still might have no way of reaching each other.
+
+A group is a collection of programs that can all communicate via pipes either directly or indirectly. The programs you identified just a moment ago are 
+all part of the same group. Now, they would like you to determine the total number of groups.
+
+In the example above, there were 2 groups: one consisting of programs 0,2,3,4,5,6, and the other consisting solely of program 1.
+
+How many groups are there in total?
 
 
 
+*/
 
--- Part 2
+-- Second Star
 
 
 WITH RECURSIVE vertices(id) AS 
@@ -51,17 +112,14 @@ WITH RECURSIVE vertices(id) AS
 
 	SELECT id_1 FROM pipes
 	UNION
-    SELECT id_2 FROM pipes
-
-
-
+	SELECT id_2 FROM pipes
 ),
 
 
  
 cte(id, branch) AS 
 (
-    SELECT id, ROW_NUMBER() OVER () FROM vertices
+        SELECT id, ROW_NUMBER() OVER () FROM vertices
 	UNION 
 	SELECT CASE WHEN id_1 != id THEN id_1 ELSE id_2 END, branch
 	FROM cte 
@@ -73,7 +131,8 @@ cte(id, branch) AS
 cte2 AS
 (
 
-	SELECT DISTINCT ON (id) id, branch FROM cte
+	SELECT DISTINCT ON (id) id, branch 
+	FROM cte
 	ORDER BY id, branch
 )
 
