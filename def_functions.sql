cea5b9f0041bc6b183d59ad418dcc7717a5a1094
Fabian Junginger <fjunginger90@gmail.com>
2023-02-28 07:25:54 +0100

    Create def_functions.sql

---
+++ def_functions.sql
@@ -0,0 +1,19 @@
+
+-- Define product aggregate function( not optimized for windows usage)
+
+
+
+
+CREATE OR REPLACE FUNCTION  MULTIPLY(BIGINT, BIGINT) RETURNS BIGINT
+    AS 'select $1 * $2;'
+    LANGUAGE SQL
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT;
+	
+	
+CREATE OR REPLACE AGGREGATE PRODUCT  EXISTS (BIGINT)
+(
+    sfunc = MULTIPLY,
+    stype = BIGINT,
+    initcond = '1'
+);
