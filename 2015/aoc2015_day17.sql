ab361a932200c08fe90c90572943ad7fc920e0cb
Fabian Junginger <fjunginger90@gmail.com>
2023-03-07 11:57:14 +0100

    Update aoc2015_day17.sql

--- 2015/aoc2015_day17.sql
+++ 2015/aoc2015_day17.sql
@@ -1,9 +1,29 @@
-SET lc_messages TO 'en_US.UTF-8';
+--- Day 17: No Such Thing as Too Much ---
+
+/*
+
+The elves bought too much eggnog again - 150 liters this time. To fit it all into your refrigerator, you'll need to move it into smaller containers. 
+You take an inventory of the capacities of the available containers.
+
+For example, suppose you have containers of size 20, 15, 10, 5, and 5 liters. If you need to store 25 liters, there are four ways to do it:
+
+   - 15 and 10
+   - 20 and 5 (the first 5)
+   - 20 and 5 (the second 5)
+   - 15, 5, and 5
+
+Filling all containers entirely, how many different combinations of containers can exactly fit all 150 liters of eggnog?
+
+*/
 
 
+-- Read data
 
 CREATE FOREIGN TABLE aoc2015_day17(x text)
 SERVER aoc2018 options(filename 'D:\aoc2015.day17.input');
+
+
+-- Create base table
 
 
 CREATE TEMPORARY TABLE containers
@@ -11,28 +31,16 @@ CREATE TEMPORARY TABLE containers
 	id SERIAL,
 	volume INT
 	
-	
 );
+
+
+-- Insert data
 
 INSERT INTO containers(volume)
 SELECT x::INT FROM aoc2015_day17;
 
 
-SELECT c1.volume + c2.volume + c3.volume AS total
-FROM containers c1
-JOIN containers c2
-	ON c1.id != c2.id
-JOIN containers c3
-	ON c2.id != c3.id
-	AND c1.id != c3.id
-ORDER BY total DESC;
-
-
-
-
-
-
--- Part 1
+-- First Star
 
 
 WITH RECURSIVE search_map(id, total_volume) AS 
@@ -53,8 +61,20 @@ WHERE total_volume = 150
 AND NOT is_cycle;
 
 
--- Part 2
+--- Part Two ---
 
+/*
+
+While playing with all the containers in the kitchen, another load of eggnog arrives! The shipping and receiving department is requesting as many containers as you can spare.
+
+Find the minimum number of containers that can exactly fit all 150 liters of eggnog. How many different ways can you fill that number of containers and 
+still hold exactly 150 litres?
+
+In the example above, the minimum number of containers was two. There were three ways to use that many containers, and so the answer there would be 3.
+*/
+
+
+-- Second Star
 
 WITH RECURSIVE search_map(id, total_volume) AS 
 (
